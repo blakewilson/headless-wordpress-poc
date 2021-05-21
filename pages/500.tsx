7eb14df4db3dea185aace227edbd77a07dc621b8
Blake Wilson <blake.wilson@wpengine.com>
2021-05-21 14:05:38 -0500

    Remove custom error pages

--- pages/500.tsx
+++
@@ -1,9 +0,0 @@
-import { headlessConfig } from "@wpengine/headless-core";
-
-headlessConfig({
-  wpUrl: process.env.NEXT_PUBLIC_WORDPRESS_URL,
-});
-
-export default function Custom500() {
-  return <h1>500 - Server-side error occurred</h1>;
-}
